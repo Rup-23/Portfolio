29bbf0e830c656e2d7a5a29be009627427853ae1
Rup-23 <152003973+Rup-23@users.noreply.github.com>
2026-03-10 12:34:10 +0530

    resume change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -341,7 +341,7 @@ const Home = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1f6suRyPCi8uxhyiI9G7Swxm53GjwuOz6/view?usp=sharing"
+                href="https://drive.google.com/file/d/19eiF6509rzUZTJtZ3g7VPwTeM38-Mr8j/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="px-8 py-3 rounded-lg font-medium border border-slate-600 bg-slate-800/50 hover:border-indigo-500/40 hover:shadow-lg hover:shadow-indigo-500/10 transition active:scale-95"
